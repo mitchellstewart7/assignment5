2bc0585e1b2d97cfcfa3618d5168a52c9fb164f9
Mitchell Stewart <stewamit@oregonstate.edu>
2022-02-13 02:31:22 -0800

    Everything pretty much works, I just don't know if it is implemented correctly. Also, the program does not work when you input pure integers they need to be in Val form (I Int). Ask me about stuff if you're confused on what I did

--- stacklang2.hs
+++ stacklang2.hs
@@ -4,7 +4,7 @@
 -- Assignment 5
 
 module StackLang where
---import Data.Maybe
+import Data.Maybe
 --import Data.Either
 
 type Prog = [Cmd]
@@ -12,7 +12,7 @@ type Prog = [Cmd]
 data Val 
     = I Int 
     | B Bool
-    deriving Show
+    deriving (Eq,Show)
 
 type Stack = [Val]
 
@@ -27,7 +27,11 @@ data Cmd
     | DEC
     | SWAP
     | POP Int
-    deriving Show
+    deriving (Eq,Show)
+
+type Rank = Int
+
+type CmdRank = (Int, Int)
 
 -- define testing stack and some tests
 stack1::Stack
@@ -43,13 +47,19 @@ test6 = [LDI 5, LDI 2, LEQ, IFELSE [LDI 10, DUP] [], ADD]
 test7 = [LDI 5, LDI 7, LEQ, IFELSE [LDI 10, DUP] [LDI 20, DUP], ADD]
 test8 = [LDI 1, LDI 2, LDI 3, LDI 4, LDI 5, ADD, ADD, ADD, ADD]
 
+run :: Prog -> Stack -> Type
+run p s = semStatTC p s
+
+--old code below, with the new functions added into semCmd
+-------------------------------------------------------------------------------
+
 -- function to run commands
-run :: Prog -> Stack -> Maybe Stack
+runOld :: Prog -> Stack -> Maybe Stack
 -- if there are no commands, then just return the original stack
-run [] s = Just s
+runOld [] s = Just s
 -- if there are commands, run the command on the first command in the array
 -- recursively run next command on the updated stack and with the tail of the array as an input
-run (x:xs) s = (semCmd x s) >>= run xs
+runOld (x:xs) s = (semCmd x s) >>= runOld xs
 
 -- function that executes a command on a stack
 semCmd :: Cmd -> Stack -> Maybe Stack
@@ -79,8 +89,8 @@ semCmd (LEQ) ((I x):(I y):xs)
 -- if it's true, run the first passed in Prog, otherwise run the second
 -- first element must be a boolean
 semCmd (IFELSE p1 p2) ((B x):xs)
-    | x==True = run p1 xs
-    | otherwise = run p2 xs
+    | x==True = runOld p1 xs
+    | otherwise = runOld p2 xs
 
 semCmd (DEC) ((I x):xs) = Just ((I (x-1)):xs)
 
@@ -91,3 +101,70 @@ semCmd (POP 0) s = Just s
 semCmd (POP k) (x:xs) = semCmd (POP (k-1)) xs
 -- catches any instance where performing a command is invalid and returns nothing
 semCmd _ _ = Nothing
+
+-- all completely new stuff
+-------------------------------------------------------------------------------------
+
+rankC :: Cmd -> CmdRank
+rankC (LDI n) = (0,1)
+rankC (LDB b) = (0,1)
+rankC (LEQ) = (2,1)
+rankC (ADD) = (2,1)
+rankC (MULT) = (2,1)
+rankC (DUP) = (1,2)
+rankC (IFELSE p1 p2) = (0,0) --temp value for IFELSE until other functions implemented
+rankC (DEC) = (1,1)
+rankC (SWAP) = (2,2)
+rankC (POP k) = (k,0)
+
+rankP :: Prog -> Rank -> Maybe Rank
+rankP [] r = Just r
+rankP (x:xs) r = (rank x r) >>= rankP xs
+
+
+rank :: Cmd -> Rank -> Maybe Rank
+rank c r
+    | checkCmdRank (rankC c) r /= -100 = Just (r - checkCmdRank (rankC c) r)
+    | otherwise = Nothing
+
+checkCmdRank :: CmdRank -> Rank -> Int
+checkCmdRank (x,y) r
+    | r >= x = x-y
+    | otherwise = -100
+
+data Type
+    = A Stack
+    | Int
+    | Bool
+    | TypeError
+    | RankError
+    deriving (Eq,Show)
+
+tcVal :: Val -> Type
+tcVal (I n) = Int
+tcVal (B b) = Bool
+
+tc :: Cmd -> Stack -> Type
+tc (LDI n) s = Int
+tc (LDB b) s = Bool
+tc (LEQ) (x:y:xs)
+    | tcVal x == Int && tcVal y == Int = Int
+tc (ADD) (x:y:xs)
+    | tcVal x == Int && tcVal y == Int = Int
+tc (MULT) (x:y:xs)
+    | tcVal x == Int && tcVal y == Int = Int
+tc (DUP) (x:xs) = tcVal x
+tc (IFELSE p1 p2) s = TypeError --placeholder until implementation
+tc (DEC) (x:xs)
+    | tcVal x == Int = Int
+tc (SWAP) s = Int --doesn't matter the types being swapped
+tc (POP k) s = Int --doesn't matter the types being popped
+tc _ _ = TypeError
+
+semStatTC :: Prog -> Stack -> Type
+semStatTC [] s = A s
+semStatTC (x:xs) s
+    | rankP (x:xs) (length s) == Nothing = RankError
+    | tc x s == TypeError = TypeError
+    | otherwise = semStatTC xs (fromJust (semCmd x s))
+
